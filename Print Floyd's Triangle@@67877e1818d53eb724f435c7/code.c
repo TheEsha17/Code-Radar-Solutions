caa50dc6af92f9f76ced47c040f567fbaf0cf2a6
Esha <esha0146.becse24@chitkara.edu.in>
2025-02-15 17:50:12 +0530

    Solution to Print Floyd's Triangle

---
+++ Print Floyd's Triangle@@67877e1818d53eb724f435c7/code.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int a,i,j;
+    scanf("%a",&a);
+    for(i=1;i<=a;i++){
+        for(j=0;j<i;j++){
+            printf("%d",1+j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
